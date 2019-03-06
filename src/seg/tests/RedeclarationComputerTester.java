0f9cbbc8bacccfffdc38b5b279eafb989ae91169
F <florence.marshall@outlook.com>
2019-03-06 00:40:42 +0000

    Just moved text on view so it didn't overlap

--- src/seg/tests/RedeclarationComputerTester.java
+++ src/seg/tests/RedeclarationComputerTester.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import seg.java.*;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RedeclarationComputerTester {
 
