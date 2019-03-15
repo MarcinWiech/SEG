27ae76558bebf22a2930de1c3c1c516a3885a267
Dan Trickey <dan@trickey.io>
2019-03-15 00:49:52 +0000

    Switch to AnchorPane

---
+++ src/seg/java/controllers/AirportConfigurationController.java
@@ -0,0 +1,17 @@
+package seg.java.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+import seg.java.models.Airport;
+
+/**
+ * The new airport configuration controller.
+ */
+public class AirportConfigurationController {
+
+
+}
