3e58144c0508f6429572bcc2f9c8cb7f86e99ae0
Dan Trickey <dan@trickey.io>
2019-03-18 00:07:18 +0000

    Back button works.

--- src/seg/java/controllers/AirportConfigurationController.java
+++ src/seg/java/controllers/AirportConfigurationController.java
@@ -4,14 +4,23 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import seg.java.models.Airport;
+import seg.java.models.Runway;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,6 +38,7 @@ public class AirportConfigurationController implements Initializable {
     @FXML public TableColumn<RunwayModel, Integer> toda;
 
     @FXML private Text airportName;
+    @FXML private Button backButton;
 
     protected Airport airport;
 
@@ -71,5 +81,28 @@ public class AirportConfigurationController implements Initializable {
         airportName.setText(airport.getName());
 
         tableView.setItems(runwayModels);
+    }
+
+    public Runway getSelectedRunway(){
+        //TODO: Fix me.
+        return new Runway("A", "B",1.0,2.0,3.0,4.0,4.0);
+    }
+
+    // Actions
+
+    public void backButtonPressed(ActionEvent actionEvent){
+        try {
+            Stage stage = (Stage) backButton.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/seg/resources/views/dashboard.fxml"));
+            Parent root1 = fxmlLoader.load();
+            stage = new Stage();
+            stage.setTitle("Dashboard");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(IOException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Uh oh, something went wrong :(").showAndWait();
+        }
     }
 }
