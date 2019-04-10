12fa4a2581847273c6d506def2afb0592abee9c9
F <florence.marshall@outlook.com>
2019-04-10 13:10:37 +0100

    PDF file created
    Have integrated the iText library - should be using Maven now (pom.xml file).
    Creates a pdf file in a new folder called outputs/pdfs.

---
+++ src/main/java/seg/java/CreatePDF.java
@@ -0,0 +1,33 @@
+package seg.java;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.*;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class CreatePDF {
+    public static final String DEST = "src/main/java/seg/outputs/pdfs/redeclared_runway.pdf";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new CreatePDF().createPDF(DEST);
+    }
+
+    public void createPDF(String DEST) throws FileNotFoundException {
+        FileOutputStream fos = new FileOutputStream(DEST);
+        PdfWriter writer = new PdfWriter(fos);
+
+        PdfDocument pdf = new PdfDocument(writer);
+
+        Document document = new Document(pdf);
+
+        document.add(new Paragraph("Wagwarn"));
+
+        document.close();
+    }
+}
