1b15905f64cfa8e8b70a490ac38fdb04897a92fd
F <florence.marshall@outlook.com>
2019-04-10 22:42:28 +0100

    Still broken :)

--- src/main/java/seg/java/CreatePDF.java
+++ src/main/java/seg/java/CreatePDF.java
@@ -68,7 +68,7 @@ public class CreatePDF {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.mail.gmail.com");
+        properties.put("mail.smtp.host", "smtp.googlemail.com");
         properties.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
